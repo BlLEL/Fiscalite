c670fe67e88f4a92c6e9c76faefa096f57354061
Bilel TOUATI <bilel83.touati@gmail.com>
2023-03-03 11:46:03 +0100

    Test Tranche 1 à Tranche 4 Valide

--- JS/Imposition.js
+++ JS/Imposition.js
@@ -6,5 +6,28 @@
 
 
 function recupMontantImposition(revenus){
-    return 0;
+    const T1 = 10777;
+    const T2 = 27478;
+    const T3 = 78570;
+    const T4 = 168994;
+    
+    if (revenus <= T1){
+        return 0;
+    }
+    else if (revenus <= T2){
+        return (revenus - T1)/100*11;
+    }
+    else if (revenus <= T3){
+        let impot = ((revenus - T2)*0.3)+((T2 - T1)*0.11);
+        return impot.toFixed(2);
+    }
+    else if (revenus <= T4){
+        let impot = ((revenus - T3)*0.41)+((T3 - T2)*0.3)+((T2 - T1)*0.11);
+        return impot.toFixed(2);
+    }
+    else if (revenus > T4){
+        let impot = ((revenus - T4)*0.45)+((T4 - T3)*0.41)+((T3 - T2)*0.3)+((T2 - T1)*0.11);
+        return impot.toFixed(2);
+    }
+    
 }
